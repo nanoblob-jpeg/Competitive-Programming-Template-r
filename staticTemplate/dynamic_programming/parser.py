1e2cda643175404423daaeb22e54b9d6e9cce35a
nanoblob-jpeg <jerry.li1248@gmail.com>
2024-07-04 15:29:00 -0700

    add knuth 2d/1d templates for both array merging type and array partition type

--- staticTemplate/dynamic_programming/parser.py
+++ staticTemplate/dynamic_programming/parser.py
@@ -2,7 +2,8 @@ def parse(fw, name, args):
     basepath = __file__.replace("\\", "/").rsplit("/", 1)[0] + "/"
     files = dict()
     files['knapsack_optimization'] = basepath+'knapsack_opt.txt'
-
+    files['knuth_array_partition'] = basepath+'knuth_array_partition.txt'
+    files['knuth_array_merging'] = basepath+'knuth_array_merging.txt'
     if name not in files:
         print("name not found here")
         exit()
